a86eb2bb13953ea93cb94fad675b0c4595fcd079
Netzer <masooglu.simon@libherr.com>
2021-11-23 11:09:48 +0100

    VectorMovement update

--- IOI2021/CAVE2/CAVE2.cpp
+++ IOI2021/CAVE2/CAVE2.cpp
@@ -147,55 +147,89 @@ unsigned char** vectorFieldOfView(std::vector<std::vector<unsigned char>> vector
 	return newFieldOfView;
 }
 
+unsigned char viewToVectorSight(char field)
+{
+	switch (field)
+	{
+		case '#':
+			return 10;
+			break;
+		case 'X':
+			return 20;
+			break;
+		case '.':
+			return 30;
+			break;
+		case 'S':
+			return 40;
+			break;
+	}
+}
+
 void vectorMoveTo(std::vector<std::vector<unsigned char>> &vectorField, unsigned char direction, int* playerPosition)
 {
 	switch (direction)
 	{
-	case 1:	// Left
-		left();
-		playerPosition[1]--;	// X--
-		if (playerPosition[1] == 0)	// Resize Vector Front
-		{
-			for (int i = 0; i < vectorField.size(); i++)
-				vectorField[i].insert(vectorField[i].begin(), 0);
+		case 1:	// Left
+			left();
+			playerPosition[1]--;	// X--
+			if (playerPosition[1] == 0)	// Resize Vector Front
+			{
+				for (int i = 0; i < vectorField.size(); i++)
+					vectorField[i].insert(vectorField[i].begin(), 0);
+				playerPosition[1]++;	// Reset Player Position because adding row led to offset
+			}
+			vectorField[playerPosition[0] - 1][playerPosition[1] - 1] = viewToVectorSight(fieldOfView[0][0]);	// X 0 0
+			vectorField[playerPosition[0]][playerPosition[1] - 1] = viewToVectorSight(fieldOfView[1][0]);		// X 0 0
+			vectorField[playerPosition[0] + 1][playerPosition[1] - 1] = viewToVectorSight(fieldOfView[2][0]);	// X 0 0
+			if (vectorField[playerPosition[0]][playerPosition[1]] % 10 == 0)
+				vectorField[playerPosition[0]][playerPosition[1]] = 43;	// Came from
+			break;
+		case 2:	// Down
+			down();
+			playerPosition[0]++;	// Y++
+			if (playerPosition[0] == (vectorField.size() - 1))	// Resize Vector Bottom
+			{
+				vectorField.insert(vectorField.end(), std::vector<unsigned char>(0));
+				for (int i = 0; i < vectorField[1].size(); i++)
+					vectorField[vectorField.size()-1].push_back(0);
+			}
+			vectorField[playerPosition[0] + 1][playerPosition[1] - 1] = viewToVectorSight(fieldOfView[2][0]);	// 0 0 0
+			vectorField[playerPosition[0] + 1][playerPosition[1]] = viewToVectorSight(fieldOfView[2][1]);		// 0 0 0
+			vectorField[playerPosition[0] + 1][playerPosition[1] + 1] = viewToVectorSight(fieldOfView[2][2]);	// X X X
+			if (vectorField[playerPosition[0]][playerPosition[1]] % 10 == 0)
+				vectorField[playerPosition[0]][playerPosition[1]] = 44;	// Came from
+			break;
+		case 3:	// Right
+			right();
+			playerPosition[1]++;	// X++
+			if (playerPosition[1] == (vectorField[0].size() - 1)) // Resize Vector Back
+			{
+				for (int i = 0; i < vectorField.size(); i++)
+					vectorField[i].insert(vectorField[i].end(), 0);
+			}
+			vectorField[playerPosition[0] - 1][playerPosition[1] + 1] = viewToVectorSight(fieldOfView[0][2]);	// 0 0 X
+			vectorField[playerPosition[0]][playerPosition[1] + 1] = viewToVectorSight(fieldOfView[1][2]);		// 0 0 X
+			vectorField[playerPosition[0] + 1][playerPosition[1] + 1] = viewToVectorSight(fieldOfView[2][2]);	// 0 0 X
+			if (vectorField[playerPosition[0]][playerPosition[1]] % 10 == 0)
+				vectorField[playerPosition[0]][playerPosition[1]] = 41;	// Came from
+			break;
+		case 4:	// Up
+			up();
+			playerPosition[0]--;	// Y--
+			if (playerPosition[0] == 0)	// Resize Vector Top
+			{
+				vectorField.insert(vectorField.begin(), std::vector<unsigned char>(0));
+				for (int i = 0; i < vectorField[1].size(); i++)
+					vectorField[0].push_back(0);
 
-			vectorField[playerPosition[0] - 1][playerPosition[1] - 1];
-		}
-		break;
-	case 2:	// Down
-		down();
-		playerPosition[0]++;	// Y++
-		if (playerPosition[0] == (vectorField.size() - 1))	// Resize Vector Bottom
-		{
-			vectorField.insert(vectorField.end(), std::vector<unsigned char>(0));
-			for (int i = 0; i < vectorField[1].size(); i++)
-				vectorField[vectorField.size()-1].push_back(0);
-
-
-		}
-		break;
-	case 3:	// Right
-		right();
-		playerPosition[1]++;	// X++
-		if (playerPosition[1] == (vectorField[0].size() - 1)) // Resize Vector Back
-		{
-			for (int i = 0; i < vectorField.size(); i++)
-				vectorField[i].insert(vectorField[i].end(), 0);
-
-
-		}
-		break;
-	case 4:	// Up
-		up();
-		playerPosition[0]--;	// Y--
-		if (playerPosition[0] == 0)	// Resize Vector Top
-		{
-			vectorField.insert(vectorField.begin(), std::vector<unsigned char>(0));
-			for (int i = 0; i < vectorField[1].size(); i++)
-				vectorField[0].push_back(0);
-
-
-		}
+				playerPosition[0]++;	// Reset Player Position because adding row led to offset
+			}
+			vectorField[playerPosition[0] - 1][playerPosition[1] - 1] = viewToVectorSight(fieldOfView[0][0]);	// X X X
+			vectorField[playerPosition[0] - 1][playerPosition[1]] = viewToVectorSight(fieldOfView[0][1]);		// 0 0 0
+			vectorField[playerPosition[0] - 1][playerPosition[1] + 1] = viewToVectorSight(fieldOfView[0][2]);	// 0 0 0
+			if (vectorField[playerPosition[0]][playerPosition[1]] % 10 == 0)
+				vectorField[playerPosition[0]][playerPosition[1]] = 42;	// Came from
 		break;
 	}
 }
@@ -210,14 +244,15 @@ void findExit(int subtask, char fieldOfView[3][3])
 	for (unsigned char i = 0; i < 3; i++)
 	{
 		std::vector<unsigned char> temp;
-		for (unsigned char i = 0; i < 3; i++)
+		for (unsigned char j = 0; j < 3; j++)
 		{
 			temp.push_back(0);
+			temp[j] = viewToVectorSight(fieldOfView[i][j]);
 		}
 		vectorField.push_back(temp);
 	}
 
-	vectorMoveTo(vectorField, 4, playerPosition);
+	vectorMoveTo(vectorField, 2, playerPosition);
 }
 
 
